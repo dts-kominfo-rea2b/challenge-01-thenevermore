cb115616b2b6447bc4ab80ef408546fbc5fd54a0
thenevermore <nurhadiarian@gmail.com>
2022-06-03 09:01:57 +0700

    done tugas

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name: 'Monica',
+    gender: 'Female',
+    age: 17,
+    email: 'monica@dingdong.com',
+    favoriteColor: ['Yellow', 'Pink', 'White', 'Purple'],
+    isHavePet: true,
+    educations: [{
+            name: 'SD 01',
+            city: 'Jakarta',
+            graduate: '2016',
+        },
+        {
+            name: 'SMP 02',
+            city: 'Jakarta',
+            graduate: '2019',
+        },
+        {
+            name: 'SMA 03',
+            city: 'Tangerang',
+        },
+    ],
+    favoriteRestaurant: ['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+};
+const secondUser = {
+    name: 'Wendy',
+    gender: 'Male',
+    age: 23,
+    email: 'wendy@dingdong.com',
+    favoriteColor: ['Blue', 'Black', 'Grey'],
+    isHavePet: false,
+    educations: [{
+            name: 'SD 02',
+            city: 'Jakarta',
+            graduate: '2010',
+        },
+        {
+            name: 'SMP 03',
+            city: 'Bogor',
+            graduate: '2013',
+        },
+        {
+            name: 'SMA 01',
+            city: 'Surabaya',
+            graduate: '2013',
+        },
+        {
+            name: 'Universitas Maju',
+            city: 'Tangerang',
+        },
+    ],
+    favoriteRestaurant: ['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']
+};
+/*
+firstUser = {
+    
+}
+
+secondUser = {
+    
+}
+*/
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [
+    firstUser,
+    secondUser
+];
+
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
